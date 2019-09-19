644c85cbe17d407ea5baacb5c59923456bfc7d16
FiorellaEffio <fiorella.effio@unmsm.edu.pe>
2019-09-19 00:59:44 -0500

    rendering clientsdata

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ import ClientsList from './components/ClientsList'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-      </header>
+      </header> */}
       <ClientsList />
     </div>
   );
--- src/components/ClientsList.js
+++ src/components/ClientsList.js
@@ -1,22 +1,31 @@
 import React, { Component } from 'react'
 import db from '../FirebaseConfig';
+import Client from './Client'
 
 class ClientsList extends Component {
     state = {
         clients: []
     }
     componentDidMount() {
+        let dataWithID;
         db.collection('clientes').get().then((snapshots) => {
             this.setState({
                 clients: snapshots.docs.map( doc => {
-                    console.log(doc.data());
+                    dataWithID = doc.data();
+                    dataWithID.id = doc.id;
+                    return dataWithID;
                 })
             })
+            console.log(this.state.clients);
         })
     }
     render() {
         return(
-            <div></div>
+            <div>
+                {this.state.clients.map(client => {
+                   return <Client key={client.id} clientData={client}/>
+                })}
+            </div>
         )
     }
 }
